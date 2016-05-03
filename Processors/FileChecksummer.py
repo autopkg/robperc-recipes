865a88b18985893718976358cd2fbde49807bb77
Robert Percival <robperc@uw.edu>
2016-05-03 13:20:46 -0700

    Added JsonReader and FileChecksummer processors

---
+++ Processors/FileChecksummer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from autopkglib import Processor, ProcessorError
+import json
+
+__all__ = ["FileChecksummer"]
+
+class StringSplitter(Processor):
+    description = "Returns MD5 checksum for specified file."
+    input_variables = {
+        "file_path": {
+            "required": True,
+            "description": "Path to file to checksum."
+        },
+    }
+    output_variables = {
+        "file_checksum": {
+            "description": "MD5 checksum of specified file.",
+        },
+    }
+    
+    __doc__ = description
+    
+    def main(self):
+        file_path = self.env["file_path"]
+        
+        self.env["file_checksum"] = hashlib.md5(open(file_path, 'rb').read()).hexdigest()
+
+if __name__ == "__main__":
+    processor = FileChecksummer()
+    processor.execute_shell()
