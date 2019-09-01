36cbd0a66a8ffe0032a61ea37cb7fb8e99b0bf62
Elliot Jordan <elliot@elliotjordan.com>
2019-09-01 16:36:41 -0700

    Make basic urlopen() calls Python 3 friendly

--- Processors/KyngChaosURLProvider.py
+++ Processors/KyngChaosURLProvider.py
@@ -17,10 +17,13 @@
 
 from __future__ import absolute_import
 import re
-import urllib2
 
 from autopkglib import Processor, ProcessorError
 
+try:
+    from urllib.parse import urlopen  # For Python 3
+except ImportError:
+    from urllib2 import urlopen  # For Python 2
 
 __all__ = ["KyngChaosURLProvider"]
 
@@ -32,7 +35,7 @@ class KyngChaosURLProvider(Processor):
     input_variables = {
         "product_name": {
             "required": True,
-            "description": 
+            "description":
                 "Product to fetch URL for. One of 'qgis', 'gdal', 'numpy', 'matplolib.",
         },
         "version": {
@@ -50,13 +53,13 @@ class KyngChaosURLProvider(Processor):
             "description": "URL to the latest KyngChaos download release.",
         },
     }
-    
+
     __doc__ = description
-    
+
     def get_kyngchaos_dmg_url(self, base_url, product_name, version):
-        
+
         product_dir = ''
-        
+
 		# Expand product name to full product name (IE: gdal -> GDAL_Complete)
 		# and get product directory from product type
         if product_name == 'qgis':
@@ -70,62 +73,62 @@ class KyngChaosURLProvider(Processor):
             product_name = 'NumPy'
         elif product_name == 'matplotlib':
             product_dir = 'python'
-        
+
         else:
             raise ValueError('product name not recognized')
-            
+
         if version == 'latest':
             version = self.get_latest_version(product_name, product_dir)
-        
-        filename = product_name + '-' + version + '.dmg'   
-        
+
+        filename = product_name + '-' + version + '.dmg'
+
 		# Construct download URL.
         dmg_url = "/".join((base_url, product_dir, filename))
-        
+
         # Try to open download link.
         try:
-            f = urllib2.urlopen(dmg_url)
+            f = urlopen(dmg_url)
             f.close()
         except BaseException as e:
             raise ProcessorError("Can't download %s: %s" % (dmg_url, e))
-        
+
         # Return URL.
         return dmg_url
-    
+
     def get_latest_version(self, product_name, product_dir):
-    
+
         # Read HTML index.
         index_url = "http://www.kyngchaos.com/software/" + product_dir
         try:
-            f = urllib2.urlopen(index_url)
+            f = urlopen(index_url)
             html = f.read()
             f.close()
         except BaseException as e:
             raise ProcessorError("Can't open %s: %s" % (index_url, e))
-        
+
         # Create regex for finding newest version
         regex = re.escape(product_name) + r'-(?P<version>[^"]+)\.dmg"'
         re_dmg = re.compile(regex)
-        
+
         # Search for newest version
         m = re_dmg.search(html)
         if not m:
             raise ProcessorError(
-                "Couldn't find %s download URL in %s" 
+                "Couldn't find %s download URL in %s"
                 % (product_name, index_url))
         # Return version
         return m.group('version')
-        
-    
+
+
     def main(self):
         # Determine base_url, version, product_name.
         product_name = self.env["product_name"]
         version = self.env.get("version", "latest")
         base_url = self.env.get("base_url", KYNGCHAOS_BASE_URL)
-        
+
         self.env["url"] = self.get_kyngchaos_dmg_url(base_url, product_name, version)
         self.output("Found URL %s" % self.env["url"])
-    
+
 
 if __name__ == "__main__":
     processor = KyngChaosURLProvider()
